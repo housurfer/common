8f586f1a33e284edfacda76729776e51135e3086
housurfer <housurfer@gmail.com>
2023-02-16 14:37:31 -0600

    name changed

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+interface person {
+  name: string,
+  address: string
+}
+
+const _person: person = {
+
+  name: 'zahid iqbal',
+  address: 'sugarland TX'
+  
+}
+
+console.log(_person)
+export default _person
